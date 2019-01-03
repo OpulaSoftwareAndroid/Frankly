741d209caf8d253fcc4f18baabeb58302ca18ea0
pruthvi <patelpruthvi2726@gmail.com>
2019-01-03 15:59:48 +0530

    close account

--- app/src/main/java/com/opula/chatapp/fragments/CreateGrpFragment.java
+++ app/src/main/java/com/opula/chatapp/fragments/CreateGrpFragment.java
@@ -3,13 +3,11 @@ package com.opula.chatapp.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -18,11 +16,8 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import com.opula.chatapp.Main2Activity;
 import com.opula.chatapp.R;
-import com.opula.chatapp.activity.MainActivity;
 import com.opula.chatapp.adapter.UserAdapter;
 import com.opula.chatapp.constant.WsConstant;
 import com.opula.chatapp.model.User;
@@ -30,19 +25,16 @@ import com.opula.chatapp.model.User;
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class ListUserFragment extends Fragment {
+public class CreateGrpFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private UserAdapter userAdapter;
     private List<User> mUsers;
     LinearLayout createNewGrpLayout;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_list_user, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_grp, container, false);
 
         recyclerView = view.findViewById(R.id.recycler_view);
 
@@ -54,68 +46,8 @@ public class ListUserFragment extends Fragment {
 
         readUsers();
 
-        createNewGrpLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Main2Activity.hideFloatingActionButton();
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.frame_mainactivity, new CreateGrpFragment()).addToBackStack(null).commit();
-            }
-        });
-
-//        search_users.addTextChangedListener(new TextWatcher() {
-//            @Override
-//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-//                searchUsers(charSequence.toString().toLowerCase());
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable editable) {
-//
-//            }
-//        });
-
         return view;
     }
-
-    private void searchUsers(String s) {
-
-        final FirebaseUser fuser = FirebaseAuth.getInstance().getCurrentUser();
-        Query query = FirebaseDatabase.getInstance().getReference("Users").orderByChild("search")
-                .startAt(s)
-                .endAt(s + "\uf8ff");
-
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                mUsers.clear();
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    User user = snapshot.getValue(User.class);
-
-                    assert user != null;
-                    assert fuser != null;
-                    if (!user.getId().equals(fuser.getUid())) {
-                        mUsers.add(user);
-                    }
-                }
-
-                userAdapter = new UserAdapter(getActivity(), mUsers, false);
-                recyclerView.setAdapter(userAdapter);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-
-    }
-
     private void readUsers() {
 
         final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
--- app/src/main/java/com/opula/chatapp/fragments/MyProfileFragment.java
+++ app/src/main/java/com/opula/chatapp/fragments/MyProfileFragment.java
@@ -1,8 +1,14 @@
 package com.opula.chatapp.fragments;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
@@ -10,12 +16,17 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -24,42 +35,59 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 import com.opula.chatapp.R;
 import com.opula.chatapp.activity.LoginRegisterActivity;
 import com.opula.chatapp.constant.AppGlobal;
+import com.opula.chatapp.model.User;
 
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
 import java.util.Objects;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 
 public class MyProfileFragment extends Fragment {
 
     ImageView imgBack;
-    LinearLayout lin_synccontact, lin_franklyweb, lin_close_account, lin_logout;
-    String loginToken, userId;
+    LinearLayout lin_close_account, lin_logout;
     FirebaseUser firebaseUser;
     DatabaseReference reference;
     FirebaseAuth firebaseAuth;
+    CircleImageView image_profile;
+    ImageView img_edit_profile;
 
+    StorageReference storageReference;
+    private StorageTask uploadTask;
+    TextView txtName, txtMobile;
+    Uri mImageUri = null;
+    int GALLERY = 1, CAMERA = 2;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_my_profile, container, false);
 
+        initViews(view);
 
-        lin_close_account = view.findViewById(R.id.lin_close_account);
-        imgBack = view.findViewById(R.id.imgBack);
-        lin_logout = view.findViewById(R.id.lin_logout);
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        storageReference = FirebaseStorage.getInstance().getReference("User_Profile");
+        checkImage();
 
-        imgBack.setOnClickListener(new View.OnClickListener() {
+        img_edit_profile.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Objects.requireNonNull(getActivity()).onBackPressed();
+            public void onClick(View view) {
+                showPictureDialog();
             }
         });
 
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         lin_close_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -74,9 +102,99 @@ public class MyProfileFragment extends Fragment {
             }
         });
 
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Objects.requireNonNull(getActivity()).onBackPressed();
+            }
+        });
+
         return view;
 
+    }
 
+    private void checkImage() {
+
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+                assert user != null;
+                txtName.setText(user.getUsername());
+                txtMobile.setText(firebaseUser.getEmail());
+                if (user.getImageURL().equals("default")) {
+                    image_profile.setImageResource(R.drawable.image_boy);
+                } else {
+                    Glide.with(getActivity()).load(user.getImageURL()).into(image_profile);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void showPictureDialog() {
+        AlertDialog.Builder pictureDialog = new AlertDialog.Builder(getContext());
+        pictureDialog.setTitle("Select Action");
+        String[] pictureDialogItems = {
+                "Gallery",
+                "Camera",
+                "Remove Photo"};
+        pictureDialog.setItems(pictureDialogItems,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        switch (which) {
+                            case 0:
+                                choosePhotoFromGallary();
+                                break;
+                            case 1:
+                                takePhotoFromCamera();
+                                break;
+                            case 2:
+                                removePhoto1();
+                                break;
+                        }
+                    }
+                });
+        pictureDialog.show();
+    }
+
+    private void removePhoto1() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+        ref.child("imageURL").setValue("default");
+        checkImage();
+    }
+
+    public void choosePhotoFromGallary() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, GALLERY);
+    }
+
+    private void takePhotoFromCamera() {
+        Intent intent1 = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent1, CAMERA);
+    }
+
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContext().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+
+    private void initViews(View view) {
+        image_profile = view.findViewById(R.id.profile_image);
+        img_edit_profile = view.findViewById(R.id.img_edit_profile);
+        lin_close_account = view.findViewById(R.id.lin_close_account);
+        imgBack = view.findViewById(R.id.imgBack);
+        lin_logout = view.findViewById(R.id.lin_logout);
+        txtName = view.findViewById(R.id.txtName);
+        txtMobile = view.findViewById(R.id.txtMobile);
     }
 
     public void showCloseAccountDialog(Context mContext) {
@@ -228,10 +346,93 @@ public class MyProfileFragment extends Fragment {
         });
     }
 
+    private void uploadImage() {
+        final ProgressDialog pd = new ProgressDialog(getContext());
+        pd.setMessage("Uploading");
+        pd.show();
 
+        if (mImageUri != null) {
+            final StorageReference fileReference = storageReference.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
 
+            uploadTask = fileReference.putFile(mImageUri);
+            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
 
+                    return fileReference.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        String mUri = downloadUri.toString();
 
+                        DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+                        HashMap<String, Object> map = new HashMap<>();
+                        map.put("imageURL", "" + mUri);
+                        reference1.updateChildren(map);
 
+                        pd.dismiss();
+                    } else {
+                        Toast.makeText(getContext(), "Failed!", Toast.LENGTH_SHORT).show();
+                        pd.dismiss();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    pd.dismiss();
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), "No image selected", Toast.LENGTH_SHORT).show();
+        }
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+        if (requestCode == GALLERY && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            if (data != null) {
+                mImageUri = data.getData();
+                image_profile.setImageURI(mImageUri);
+                if (uploadTask != null && uploadTask.isInProgress()) {
+                    Toast.makeText(getContext(), "Upload in preogress", Toast.LENGTH_SHORT).show();
+                } else {
+                    uploadImage();
+                }
+            }
+        }
+        if (requestCode == CAMERA && resultCode == RESULT_OK) {
+
+            Bundle bundle = data.getExtras();
+            final Bitmap bitmap = (Bitmap) bundle.get("data");
+            mImageUri = getImageUri(getContext(), bitmap);
+            image_profile.setImageBitmap(bitmap);
+            if (uploadTask != null && uploadTask.isInProgress()) {
+                Toast.makeText(getContext(), "Upload in preogress", Toast.LENGTH_SHORT).show();
+            } else {
+                uploadImage();
+            }
+        }
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(),
+                inImage, "Title", null);
+        return Uri.parse(path);
+    }
 }
